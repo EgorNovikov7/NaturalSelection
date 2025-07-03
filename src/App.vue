6312e45277d15f5277ee34207b7f2ae2089f6dc1
Egor Novikov <nem03062002@gmail.com>
2025-07-03 15:14:03 +0700

    Добавил размножение и поедание

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <canvas ref="canvas" width="700" height="700"></canvas>
-    <div>Хищников: {{ predatorsCount }}, Жертв: {{ preysCount }}</div>
+    <div>
+      Хищников: {{ predatorsCount }}, Жертв: {{ preysCount }}
+      <br>Съедено: {{ eatenCount }}, Родилось: {{ bornCount }}
+    </div>
   </div>
 </template>
 
@@ -16,6 +19,7 @@ class Creature {
     this.type = type
     this.dx = Math.random() * 2 - 1
     this.dy = Math.random() * 2 - 1
+    this.energy = 100
   }
 
   move() {
@@ -24,12 +28,20 @@ class Creature {
     
     if (this.x < 0 || this.x > 700) this.dx *= -1
     if (this.y < 0 || this.y > 700) this.dy *= -1
+    
+    this.energy -= 0.1
+    return this.energy > 0
+  }
+
+  distanceTo(other) {
+    return Math.sqrt((this.x - other.x) ** 2 + (this.y - other.y) ** 2)
   }
 }
 
 class Predator extends Creature {
   constructor(x, y) {
     super(x, y, 20, 1.5, 'red', 'predator')
+    this.eaten = 0
   }
 }
 
@@ -44,7 +56,9 @@ export default {
     return {
       ctx: null,
       creatures: [],
-      animationId: null
+      animationId: null,
+      eatenCount: 0,
+      bornCount: 0
     }
   },
   computed: {
@@ -59,14 +73,13 @@ export default {
     const canvas = this.$refs.canvas
     this.ctx = canvas.getContext('2d')
     
-    // 5 жертв и 2 хищника
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       this.creatures.push(new Prey(
         Math.random() * 700,
         Math.random() * 700
       ))
     }
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 3; i++) {
       this.creatures.push(new Predator(
         Math.random() * 700,
         Math.random() * 700
@@ -78,12 +91,75 @@ export default {
   methods: {
     startAnimation() {
       const animate = () => {
-        this.creatures.forEach(c => c.move())
+        this.updateCreatures()
+        this.checkCollisions()
         this.draw()
         this.animationId = requestAnimationFrame(animate)
       }
       animate()
     },
+    
+    updateCreatures() {
+      const newCreatures = []
+      const deadCreatures = []
+      
+      this.creatures.forEach((creature, index) => {
+        if (!creature.move()) {
+          deadCreatures.push(index)
+        }
+        
+        // Размножение при достаточной энергии
+        if (creature.energy > 150) {
+          creature.energy = 50
+          newCreatures.push(this.reproduce(creature))
+          this.bornCount++
+        }
+      })
+      
+      // Удаляем умерших
+      deadCreatures.reverse().forEach(index => {
+        this.creatures.splice(index, 1)
+      })
+      
+      // Добавляем новых
+      this.creatures.push(...newCreatures)
+    },
+    
+    checkCollisions() {
+      for (let i = 0; i < this.creatures.length; i++) {
+        for (let j = i + 1; j < this.creatures.length; j++) {
+          const a = this.creatures[i]
+          const b = this.creatures[j]
+          
+          if (a.distanceTo(b) < a.size + b.size) {
+            if (a.type === 'predator' && b.type === 'prey') {
+              this.eat(a, b)
+            } else if (b.type === 'predator' && a.type === 'prey') {
+              this.eat(b, a)
+            }
+          }
+        }
+      }
+    },
+    
+    eat(predator, prey) {
+      predator.energy += 50
+      predator.eaten++
+      this.eatenCount++
+      
+      const index = this.creatures.indexOf(prey)
+      if (index !== -1) {
+        this.creatures.splice(index, 1)
+      }
+    },
+    
+    reproduce(parent) {
+      return new parent.constructor(
+        parent.x + (Math.random() * 40 - 20),
+        parent.y + (Math.random() * 40 - 20)
+      )
+    },
+    
     draw() {
       this.ctx.clearRect(0, 0, 700, 700)
       this.creatures.forEach(c => {
